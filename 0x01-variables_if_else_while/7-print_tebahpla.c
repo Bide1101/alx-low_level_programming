a2808b62ab2d715e5be65a9f350a6bbc4ed69b4d
Bide1101 <olabiyibisolad@gmail.com>
2023-02-17 14:30:27 -0800

    Mandatory

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char lr;
 
-	for (lr = 'z'; lr <= 'z'; lr--)
+	for (lr = 'z'; lr <= 'a'; lr--)
 		putchar(lr);
 
 	putchar('\n');
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - prints all possible combinations of single-digit numbers
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 28; num < 38; num++)
+
+	{
+		putchar(num);
+		if (num != 37)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
