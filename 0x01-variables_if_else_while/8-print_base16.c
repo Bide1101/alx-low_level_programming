8cc38619db8036174977d8b3020d06bb72d88ede
Bide1101 <olabiyibisolad@gmail.com>
2023-02-17 14:01:01 -0800

    mirror

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - prints alpha lowercase in reverse order
+* Return: Always 0
+*/
+
+int main(void)
+{
+	char lr;
+
+	for (lr = 'z'; lr <= 'z'; lr--)
+		putchar(lr);
+
+	putchar('\n');
+
+	return (0);
+}
