ab7ec41fc1550ce9be24760eb50caf68176b6eab
Bide1101 <olabiyibisolad@gmail.com>
2023-02-21 14:58:24 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,14 @@
  */
 void print_to_98(int n)
 {
-	if (n-- <= 98)
+	if (n-- >= 98)
 	{
-		printf("%d, ", n);
+		printf("%d, ", n--);
 	printf("\n");
 	}
-	else if (n++ >= 98)
+	else if (n++ <= 98)
 	{
-		printf("%d, ", n);
+		printf("%d, ", n++);
 	printf("\n");
 	}
 }
