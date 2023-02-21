d5a504493ae87e757ebf42b133750a7e226170b5
Bide1101 <olabiyibisolad@gmail.com>
2023-02-21 13:30:26 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints last digit of a number
+* @l: integer checked
+* Return: last digit (success)
+*/
+
+int print_last_digit(int l)
+{	l = l  % 3;
+		if (l <= 0)
+		return (l);
+}
