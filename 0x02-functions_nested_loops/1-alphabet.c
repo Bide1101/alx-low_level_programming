9e414c5fcbbea8229c5dfb978923c7490a6c6255
Bide1101 <olabiyibisolad@gmail.com>
2023-02-21 07:01:58 -0800

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - a function that prints the alphabet, in lowercase
+*followed by a new line 
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+
+	return (0);
+}
