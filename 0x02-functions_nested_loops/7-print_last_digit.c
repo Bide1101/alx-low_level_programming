9eb9974f148659c39928c53a64297782c21253d9
Bide1101 <olabiyibisolad@gmail.com>
2023-02-21 13:41:19 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,13 @@
 /**
 * print_last_digit - prints last digit of a number
 * @l: integer checked
-* Return: l
+* Return: 0 (success)
 */
 
 int print_last_digit(int l)
 {	l = l  % 10;
 		if (l <= 0)
+
 		_putchar (l);
+		return (0);
 }
