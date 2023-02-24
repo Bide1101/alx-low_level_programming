1fd15c638314a7c26abe68d50f3f972e8b71cef7
Bide1101 <olabiyibisolad@gmail.com>
2023-02-24 03:12:51 -0800

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - a program that multiplies two integers
+* @a: first integer
+* @b: second integer
+* Return: (0) always
+*/
+
+int mul(int a, int b)
+{
+	int a, b, mult;
+
+	for (a * b == mult)
+		_putchar (mult);
+
+	return (0);
+}
