cfb4b90025e19ca5e5c5f4d1c92008d162604e05
Bide1101 <olabiyibisolad@gmail.com>
2023-02-24 02:32:42 -0800

    TASK 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _isupper - prints uppercase
+* @c: letter
+* Return: return 1 if uppercase otherwise return 0
+*/
+
+int _isupper(int c)
+{
+	int c = 'C';
+	
+	if (c == 'C')
+		return (1);
+	else
+		return (0);
+
