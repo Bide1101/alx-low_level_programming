e75acf421102987d75ceff9a9b128de0c13e27f8
Bide1101 <olabiyibisolad@gmail.com>
2023-02-24 04:36:52 -0800

    task 6

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
 * print_numbers - print numbers from 0 - 9
-* Return: void
+* Return: (0) always
 */
 
 void print_numbers(void)
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - printing a straight lines
+ * @n: number of times
+ * Return: 0 (success)
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
