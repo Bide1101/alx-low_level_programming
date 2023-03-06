09587859067324da95106955027c752a5d917091
Bide1101 <olabiyibisolad@gmail.com>
2023-03-06 15:11:56 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 
 	while (haystack[i])
 	{
-		for (;needle[j] && (haystack[i] == needle[0]);)
+		for (; needle[j] && (haystack[i] == needle[0]);)
 		{
 			if (haystack[i + j] == needle[j])
 				j++;
