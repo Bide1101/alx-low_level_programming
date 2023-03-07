fc2398883dddee0fbb49348c052c1e93bab4fef7
Bide1101 <olabiyibisolad@gmail.com>
2023-03-07 14:24:06 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+* _puts_recursion -  prints a string.
+* @s: string
+*/
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	if (s[i] == '/0')
+		_putchar(10);
+
+	for (i = 0; i< s[i]; i++)
+		_putchar(s[i]);
+}
