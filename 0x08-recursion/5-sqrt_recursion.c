e531794001a9f8c9bdcb916b4c8ff2150d7b081a
Bide1101 <olabiyibisolad@gmail.com>
2023-03-08 14:41:19 -0800

    task betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+* sq - checks for the square root
+* @a:integer
+* @g:integer
+* Return: integer
+*/
+
 int sq(int a, int g)
 
 {
@@ -10,16 +17,24 @@ int sq(int a, int g)
 		return (-1);
 
 	else
-		return sq(a + 1,g);
+		return (sq(a + 1, g));
 }
+
+/**
+* _sqrt_recursion - natural square root of a number
+* @n: integer value
+* Return: integer
+*/
+
 int _sqrt_recursion(int n)
 {
-	 if (n == 0)
-	{
+	if (n == 0)
+		{
 		return (0);
-	}
+		}
+
 	else
-	{
-		return sq(1,n);
-	}
+		{
+		return (sq(1, n));
+		}
 }
