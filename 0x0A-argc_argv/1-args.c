030d9105b168fb28d9f83920b369bc3196a332f2
Bide1101 <olabiyibisolad@gmail.com>
2023-03-10 14:52:53 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - prints the number of arguments passed into it
+* @argc: argument count
+* @argv: string
+* Return: 0 Always
+*/
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("argc = %d\n", argc);
+	return (0);
+}
