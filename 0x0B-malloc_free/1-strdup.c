f1813702e5bc98cb563711eb560747fb5c2b49fa
Bide1101 <olabiyibisolad@gmail.com>
2023-03-13 15:39:28 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,20 +12,21 @@ char *_strdup(char *str)
 	char *ptr;
 	int n = 0, i;
 
-	ptr = (char *)malloc(sizeof(str) * sizeof(char) + 1);
-	if (ptr == NULL)
+	if (str == 0)
 		return (NULL);
 
 	while (str[n] != '\0')
 		n++;
 
+	ptr = (char *)malloc(n * sizeof(char) + 1);
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < n; i++)
 	{
 		ptr[i] = str[i];
 	}
-
-	if (str == 0)
-		return (NULL);
-	else
+		ptr[n] = '\0';
+	
 		return (ptr);
 }
