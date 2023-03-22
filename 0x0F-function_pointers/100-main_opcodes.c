b409e0cd6d83e9e218de2146533924c67cfda0f3
Bide1101 <olabiyibisolad@gmail.com>
2023-03-22 09:04:53 -0700

    print opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -34,17 +34,17 @@ int main(int argc, char *argv[])
 {
 	int bytes;
 
-	if (argc <  2)
+	if (argc !=  2)
 	{
 		printf("Error\n");
-		exit (1);
+		exit(1);
 	}
 
 	bytes = atoi(argv[1]);
 	if (bytes < 0)
 	{
 		printf("Error\n");
-		exit (2);
+		exit(2);
 	}
 
 	print_opcodes(bytes);
