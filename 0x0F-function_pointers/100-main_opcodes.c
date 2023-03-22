8bd8a1fe0535a6dee0f5e3c8ddcef291d795503c
Bide1101 <olabiyibisolad@gmail.com>
2023-03-22 08:31:23 -0700

    print opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* print_opcodes - prints opcodes
+* @nbytes: number of bytes
+* Return: void
+*/
+
+void print_opcodes(int nbytes)
+{
+	unsigned char *p;
+	int i;
+
+	p = (unsigned char *)print_opcodes;
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x ", p[i]);
+	}
+	printf("\n");
+}
+
+/**
+* main - controls how the program is called
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 Always
+*/
+
+int main(int argc, char *argv[])
+{
+	int bytes;
+
+	if (argc <  2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes <= 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	print_opcodes(bytes);
+	return (0);
+}
+
