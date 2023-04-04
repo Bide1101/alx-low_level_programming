4cc3d63c164cb83a6756aa31452f30d80478863e
Bide1101 <olabiyibisolad@gmail.com>
2023-04-03 18:12:37 -0700

    fixed errors

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,12 +17,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-
 	if (idx != 0)
 	{
 		tmp = *head;
@@ -32,11 +26,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			count++;
 		}
 		if (tmp == NULL)
-		{
-			free(new_node);
 			return (NULL);
-		}
 	}
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
 	if (idx == 0)
 	{
 		new_node->next = *head;
